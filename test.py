d82beaa4236069240c285ceba9fec3c2a72a80be
Apiros3 <akikan.contact@gmail.com>
2023-05-16 18:27:59 +0100

    update

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
-with open('readme.txt', 'w') as f:
+with open('readme.md', 'w') as f:
     f.write(f"Last Update = {current_time}\n")
